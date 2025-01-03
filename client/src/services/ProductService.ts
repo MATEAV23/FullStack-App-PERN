f57345b56b80c2bbadd015dc436b2f599e6b95b6
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-01-03 14:24:32 -0300

    validando la data enviada a la peticion

--- client/src/services/ProductService.ts
+++ client/src/services/ProductService.ts
@@ -1,7 +1,22 @@
+import { safeParse } from 'valibot'
+import { DraftProductSchema } from '../types'
+
 type ProducData = {
     [k: string]: FormDataEntryValue;
 }
 
 export async function addProduct(data : ProducData) {
-    console.log(data)
+    try {
+        const result = safeParse(DraftProductSchema, {
+            name: data.name,
+            price: +data.price
+        })
+        if(result.success) {
+            
+        } else {
+            throw new Error('Datos no validos')
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
