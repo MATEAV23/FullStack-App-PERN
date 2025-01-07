10b16f96d52e90f2d7b9ff3f0d508e796e3f3dc7
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-01-07 15:07:10 -0300

    Agregando funcion de actualizar disponibilidad

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,5 +1,5 @@
 import type { Product } from "../types"
-import { Form, useNavigate, ActionFunctionArgs, redirect } from 'react-router-dom'
+import { Form, useNavigate, ActionFunctionArgs, redirect, useFetcher } from 'react-router-dom'
 import { formatCurrency } from "../utils"
 import { deleteProduct } from "../services/ProductService"
 
@@ -21,6 +21,7 @@ export async function action({params} : ActionFunctionArgs) {
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
 
+    const fetcher = useFetcher()
     const navigate = useNavigate()
 
     const isAvailable = product.availability
@@ -34,7 +35,19 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
                 { formatCurrency(product.price)}
             </td>
             <td className="p-3 text-lg text-gray-800">
-                {isAvailable ? 'Disponible' : 'No Disponible'}
+
+                <fetcher.Form method="POST">
+                    <button
+                        type="submit"
+                        name="id"
+                        value={product.id}
+                        className={`${isAvailable ? 'text-black' : 'text-red-600'} rounded-lg p-2 text-xs uppercase font-bold w-full border
+                        border-black-100 hover:cursor-pointer
+                        `}
+                    >
+                        {isAvailable ? 'Disponible' : 'No Disponible'}
+                    </button>
+                </fetcher.Form>     
             </td>
             <td className="p-3 text-lg text-gray-800 ">
                 <div className="flex gap-2 items-center">
